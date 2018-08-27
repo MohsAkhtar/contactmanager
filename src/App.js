0d5196e36ffb148ffa3549035c8706bd6b7eea49
40183396 <40183396@live.napier.ac.uk>
2018-08-27 20:54:53 +0100

    Added style to header, contact and main page

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="johndoe@email.com" phone="123456789" />
-        <Contact
-          name="Karen Armstrong"
-          email="karmstrong@email.com"
-          phone="111222333"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="johndoe@email.com"
+            phone="123456789"
+          />
+          <Contact
+            name="Karen Armstrong"
+            email="karmstrong@email.com"
+            phone="111222333"
+          />
+        </div>
       </div>
     );
   }
