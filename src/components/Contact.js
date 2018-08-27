f3c0121ca2d0a0e319fb18b7a892aac53ee0e676
40183396 <40183396@live.napier.ac.uk>
2018-08-27 20:12:50 +0100

    Added Contact and header components

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>John Doe</h4>
+        <ul>
+          <li>Email: johndoe@email.com</li>
+          <li>Phone: 123456789</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
